aa55a874b40d0b3987c27bd8693f54e74e609091
RitikFS <ritikkumarsingh647@gmail.com>
2025-07-18 11:35:27 +0530

    Updated components: added Contact section, improved navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // <--- THIS IS IMPORTANT
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
